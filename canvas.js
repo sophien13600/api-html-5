0aa87635c9a21b362c69b5578d33c54b85ab5a88
Sophien <bouderbala.s@gmail.com>
2025-07-30 16:57:14 +0200

    correction

--- canvas.js
+++ canvas.js
@@ -39,3 +39,42 @@ function deplacerHaut(){
 
     dimensions[1]-= 10
 }
+/**
+ * 
+ * 
+ * const div = document.getElementById('canvas')
+const context = div.getContext("2d")
+
+const step = 10
+
+
+const dimensions = [20, 20, 100, 20]
+
+context.fillStyle = 'blue'
+
+context.fillRect(...dimensions)
+
+function clearCanvas() {
+    context.clearRect(...dimensions)
+}
+function right() {
+    console.log(step + dimensions[0] + dimensions[2], div.width);
+
+    if (step + dimensions[0] + dimensions[2] <= div.width) {
+        context.clearRect(...dimensions)
+
+        dimensions[0] += step
+        context.fillRect(...dimensions)
+        console.log(dimensions);
+    } else {
+        context.fillStyle = 'red'
+        setTimeout(
+            () => context.fillStyle = 'blue', 
+            2000
+        )
+    }
+
+
+}
+
+ */
